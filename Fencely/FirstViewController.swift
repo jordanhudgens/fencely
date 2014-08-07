67680826afc9e7d2fb90b2ff16dc3af39a15e3b4
Jordan Hudgens <jordan@wow.com>
2014-08-07 09:40:53 -0700

    initial commit

---
+++ Fencely/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Fencely
+//
+//  Created by Jordan Hudgens on 8/7/14.
+//  Copyright (c) 2014 Jordan Hudgens. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
