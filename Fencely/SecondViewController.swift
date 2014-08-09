d2c8479654c4d4dbdf67f76f6850bddfd752b5bf
Jordan Hudgens <jordan@wow.com>
2014-08-09 15:37:29 -0700

    working on building in the location

--- Fencely/SecondViewController.swift
+++ Fencely/SecondViewController.swift
@@ -8,20 +8,104 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class SecondViewController: UIViewController {
+// Integrate two Plist items: http://stackoverflow.com/questions/24252645/how-to-get-location-user-whith-cllocationmanager-in-swift
+
+class SecondViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    var location: CLLocation!
+    
+    @IBOutlet var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Make this controller the delegate for the map view.
+        self.mapView.delegate = self
+        
+        println("Location is: \(location)")
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    var window: UIWindow?
+    var locationManager: CLLocationManager!
+    var seenError : Bool = false
+    var locationFixAchieved : Bool = false
+    var locationStatus : NSString = "Not Started"
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: NSDictionary?) -> Bool {
+        initLocationManager();
+        return true
+    }
+    
+    // Location Manager helper stuff
+    func initLocationManager() {
+        seenError = false
+        locationFixAchieved = false
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        locationManager.requestAlwaysAuthorization()
+    }
+    
+    // Location Manager Delegate stuff
+    
+    func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {
+        locationManager.stopUpdatingLocation()
+        if ((error) != nil) {
+            if (seenError == false) {
+                seenError = true
+                print(error)
+            }
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        if (locationFixAchieved == false) {
+            locationFixAchieved = true
+            var locationArray = locations as NSArray
+            var locationObj = locationArray.lastObject as CLLocation
+            var coord = locationObj.coordinate
+            
+            println(coord.latitude)
+            println(coord.longitude)
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager!,
+        didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+            var shouldIAllow = false
+            
+            switch status {
+            case CLAuthorizationStatus.Restricted:
+                locationStatus = "Restricted Access to location"
+            case CLAuthorizationStatus.Denied:
+                locationStatus = "User denied access to location"
+            case CLAuthorizationStatus.NotDetermined:
+                locationStatus = "Status not determined"
+            default:
+                locationStatus = "Allowed to location Access"
+                shouldIAllow = true
+            }
+            NSNotificationCenter.defaultCenter().postNotificationName("LabelHasbeenUpdated", object: nil)
+            if (shouldIAllow == true) {
+                NSLog("Location to Allowed")
+                // Start location services
+                locationManager.startUpdatingLocation()
+            } else {
+                NSLog("Denied access: \(locationStatus)")
+            }
+    }
 
 
-    @IBOutlet var mapView: MKMapView!
+    
 }
 
